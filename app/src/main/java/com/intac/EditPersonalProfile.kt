8cc0f43559c85eed06d1f59d44beb6fb4c2c38a0
IOANNVOLZHSKIY <ivantokarevd@gmail.com>
2022-11-04 20:58:17 +0300

    Add personal and business settings layout

---
+++ app/src/main/java/com/intac/EditPersonalProfile.kt
@@ -0,0 +1,11 @@
+package com.intac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPersonalProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_personal_profile)
+    }
+}
